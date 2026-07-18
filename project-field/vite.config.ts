9560aaedee67764ed24a0f7f554eb27e4576747e
CoolGandalf <leigh.llewelyn@gmail.com>
2026-07-18 11:54:00 -0400

    Fix Project Field production rendering (#4)
    Co-authored-by: CoolGandalf <261268531+CoolGandalf@users.noreply.github.com>

--- project-field/vite.config.ts
+++ project-field/vite.config.ts
@@ -4,6 +4,9 @@ import { fileURLToPath, URL } from "node:url";
 export default defineConfig({
   root: fileURLToPath(new URL(".", import.meta.url)),
   base: "/wikileighs/project-field/",
+  esbuild: {
+    jsx: "automatic",
+  },
   build: {
     outDir: fileURLToPath(new URL("../public/project-field", import.meta.url)),
     emptyOutDir: true,
